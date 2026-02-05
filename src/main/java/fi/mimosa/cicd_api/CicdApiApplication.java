d7f0818586824aa8dcbac05861b9d43f504ee43f
Mimosa Ylinen <ylinen.mimosa@gmail.com>
2026-02-05 18:56:53 +0200

    Initial commit

---
+++ src/main/java/fi/mimosa/cicd_api/CicdApiApplication.java
@@ -0,0 +1,13 @@
+package fi.mimosa.cicd_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/mimosa/cicd_api/CicdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.mimosa.cicd_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
